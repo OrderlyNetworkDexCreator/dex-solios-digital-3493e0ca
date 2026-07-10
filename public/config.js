d82fd7f7377455eb0fd7706a69a0770d14df6770
orderly-guoqing <guoqing@orderly.network>
2026-07-11 00:02:03 +0800

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -27,7 +27,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_TWITTER_URL": "",
   "VITE_SEO_SITE_NAME": "",
   "VITE_SEO_SITE_DESCRIPTION": "",
-  "VITE_SEO_SITE_URL": "https://dex.orderly.network/dex-solios-digital-3493e0ca/",
+  "VITE_SEO_SITE_URL": "https://solios.digital",
   "VITE_SEO_SITE_LANGUAGE": "en",
   "VITE_SEO_SITE_LOCALE": "en_US",
   "VITE_SEO_TWITTER_HANDLE": "",
